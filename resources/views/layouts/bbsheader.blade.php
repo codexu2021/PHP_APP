4bfdd0f04fdaf88bfc69d9757553ea1be38a43c6
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-17 02:26:51 +0900

    ログイン後の処理

--- resources/views/layouts/bbsheader.blade.php
+++ resources/views/layouts/bbsheader.blade.php
@@ -6,7 +6,7 @@
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
         @auth
-            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+            <a href="{{ url('/index') }}" class="text-sm text-gray-700 underline">掲示板</a>
         @else
             <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
