c4537114eadac8cd89a97f953c5ddde8afd16979
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-17 03:53:24 +0900

    create調整

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Http\Requests;
 
 class PostController extends Controller
 {
