b4669052b1b07ebf42bff7bec31b48dca1f4e8fc
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-17 04:23:19 +0900

    fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillabel = [
+    protected $fillable = [
         'name',
         'subject',
         'message',
