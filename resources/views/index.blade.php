e7da7391c6d66a00df509962acb1a27f850882f5
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-16 13:43:27 +0900

    確認

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
-@extends('layout.bbslayout')
+@extends('bbslayout')
 @section('title', 'LaravelPjt BBS 投稿の一覧ページ')
 @section('keywords', 'キーワード1,キーワード2,キーワード3')
 @section('description', '投稿一覧ページの説明文')
 @section('pageCss')
 <link href="/css/bbs/style.css" rel="stylesheet">
 @endsection
-@include('layout.bbsheader')
+@include('bbsheader')
 @section('content')
 <div class="table-responsive">
     <table class="table table-hover">
@@ -49,4 +49,4 @@
 </div>
 </div>
 @endsection
-@include('layout.bbsfooter')
+@include('bbsfooter')
