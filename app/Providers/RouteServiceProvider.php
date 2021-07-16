e08bb86d925b55cdf65239260f6b6e121a95eb48
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-16 22:29:36 +0900

    ログイン後のURL変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/index';
+    public const HOME = '\Post.index';
 
     /**
      * The controller namespace for the application.
