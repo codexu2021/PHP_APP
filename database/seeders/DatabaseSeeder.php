0bb4b89cd1d4b3dd394dd44f97ca779cb649e54f
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-16 23:38:42 +0900

    テストデータ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(PostsTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
