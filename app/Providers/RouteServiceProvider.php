12475c20fc44da818e48f255fe68e92e3da4631d
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-17 02:05:32 +0900

    パスの変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'Post\index';
+    public const HOME = 'index';
 
     /**
      * The controller namespace for the application.
